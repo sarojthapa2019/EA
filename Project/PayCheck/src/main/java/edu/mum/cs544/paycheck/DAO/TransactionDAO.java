a424b9e81052bb9495f9abfb275ee9609b459509
Sujiv Shrestha <sujivshrestha@yahoo.com>
2019-07-19 23:33:38 -0500

    After completing EA project

--- Project/PayCheck/src/main/java/edu/mum/cs544/paycheck/DAO/TransactionDAO.java
+++
@@ -1,9 +0,0 @@
-package edu.mum.cs544.paycheck.DAO;
-
-import edu.mum.cs544.paycheck.Domain.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionDAO extends JpaRepository<Transaction, Integer> {
-}
